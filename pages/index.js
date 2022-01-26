b25061184a7b54463fd5cb7f9f52ec975b567d54
José Luan Lima da Costa <joseluan620@gmail.com>
2022-01-25 21:44:18 -0300

    Projeto criado e com um pouco de estilização

---
+++ pages/index.js
@@ -0,0 +1,180 @@
+import appConfig from '../config.json';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+//Criação de uma tag de estilos global
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+    );
+}
+
+
+//Criação de uma tag que recebe como parametro o conteudo de onde ela é chamada
+//Jutando todos as linguagens em um componente HTML, CSS E JS
+function Titulo(props) {
+    console.log(props)
+    const Tag = props.tag || 'h1';
+    return (
+        //Deixando o codigo mais variavel a partir do parametro props, 
+        //para facilitar a manuntenção dele
+
+        <>{/*tag vazia para que seja possivel usar o <style jsx>*/}
+            <Tag>{props.children}</Tag>{/*props.children resgada o valor de onde a tag foi usada*/}
+            {/*<style jsx>: Estilo de css especifico para essa tag, 
+            ele cria uma classe para esse tag e faz toda a associação*/}
+            <style jsx>{`
+                ${Tag} {
+                    color: ${appConfig.theme.colors.neutrals['200']};
+                    font-size: 24px;
+                    font-weight: 600; 
+                }
+            `}</style>
+        </>
+
+    );
+}
+
+
+//Componente react 
+// function HomePage() {
+//     return (/*  */
+//         <div>
+//             <GlobalStyle />{/*chamada da tag global de estilos*/}
+//             <Titulo tag="h2">Boas vindas de volta</Titulo>
+//             <h2>Dicord - Alura Matrix</h2>
+//         </div>)
+// }
+
+// export default HomePage
+
+export default function PaginaInicial() {
+    const username = 'jose-luan19';
+
+    return (
+        <>
+            <GlobalStyle />
+            <Box
+                styleSheet={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    backgroundColor: appConfig.theme.colors.primary[500],
+                    backgroundImage: 'url(https://t.ctcdn.com.br/kdJP2DeoupkKzlgYmRszZ_SzbXw=/0x40:1728x1013/1400x788/smart/i325991.jpeg)',
+                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: {
+                            xs: 'column',
+                            sm: 'row',
+                        },
+                        width: '100%', maxWidth: '700px',
+                        borderRadius: '5px', padding: '32px', margin: '16px',
+                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }}
+                >
+                    {/* Formulário */}
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                            width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                        }}
+                    >
+                        <Titulo tag="h2">Boas vindas de volta!</Titulo>
+                        <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                            {appConfig.name}
+                        </Text>
+
+                        <TextField
+                            fullWidth
+                            textFieldColors={{
+                                neutral: {
+                                    textColor: appConfig.theme.colors.neutrals[200],
+                                    mainColor: appConfig.theme.colors.neutrals[900],
+                                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                                },
+                            }}
+                        />
+                        <Button
+                            type='submit'
+                            label='Entrar'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[700],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                            }}
+                        />
+                    </Box>
+                    {/* Formulário */}
+
+
+                    {/* Photo Area */}
+                    <Box
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            maxWidth: '200px',
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            flex: 1,
+                            minHeight: '240px',
+                        }}
+                    >
+                        <Image
+                            styleSheet={{
+                                borderRadius: '50%',
+                                marginBottom: '16px',
+                            }}
+                            src={`https://github.com/${username}.png`}
+                        />
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: appConfig.theme.colors.neutrals[200],
+                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                padding: '3px 10px',
+                                borderRadius: '1000px'
+                            }}
+                        >
+                            {username}
+                        </Text>
+                    </Box>
+                    {/* Photo Area */}
+                </Box>
+            </Box>
+        </>
+    );
+}
